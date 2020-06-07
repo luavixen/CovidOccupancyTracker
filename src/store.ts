fb32db8329b7b332d5150c2d391d83d60e1d0c5a
Lua MacDougall <luawhat@gmail.com>
2020-06-06 18:04:44 -0700

    Added database functionality to store
    The store is now finished, it will automatically handle all the Firestore nonsense and keep us in-sync with the database.

--- src/store.ts
+++ src/store.ts
@@ -15,8 +15,8 @@ export const locationCollection = firebase.firestore().collection("locations");
 /** LocalStorage key that contains the last valid ID */
 export const localStorageIDKey = "covid-occupancy-tracker-last-id";
 
-/** Small utility function to convert any (non-NaN) number to a natural number */
-export const asNatural = (num: number) =>
+/** Small utility function to convert any (non-NaN) number to a whole number */
+export const asWhole = (num: number) =>
 	Math.max(Math.floor(num), 0);
 
 /** In-database representation of location data */
@@ -69,9 +69,9 @@ export const store = new Vuex.Store<State>({
 	getters: {
 	},
 	mutations: {
-		/** Set the `.location.data.people` field to a natural number */
+		/** Set the `.location.data.people` field to a whole number */
 		[Mutation.SetPeople](state, people: State["location"]["data"]["people"]) {
-			state.location.data.people = asNatural(people);
+			state.location.data.people = asWhole(people);
 		},
 		/** Set the `.location.id` field to a valid ID or `null` */
 		[Mutation.SetID](state, id: State["location"]["id"]) {
@@ -105,9 +105,164 @@ export const store = new Vuex.Store<State>({
 			} catch (err) {
 				console.error(`Failed to read "${localStorageIDKey}" from LocalStorage:`, err);
 			}
+		},
+		/** "Connect" to a location in the database and stay in sync */
+		[Action.LocationConnect]: async (store, id: string) => {
+			/* Guard against invalid IDs (currently accepting all truthy strings) */
+			if (typeof id !== "string" || !id) {
+				throw new Error("Invalid ID");
+			}
+
+			/* Disconnect from the current location (if we are connected) */
+			await store.dispatch(Action.LocationDisconnect);
+
+			/* Update the store's `.location.id` */
+			store.commit(Mutation.SetID, id);
+
+			/* Get a reference to the document for this location */
+			const ref = locationCollection.doc(id);
+
+			/* Gonna have to get a bit messy to make this work nicely,
+				using a Promise we can async-await-ify the onSnapshot system */
+
+			try {
+				await new Promise((resolve, reject) => {
+					/* Attempt to subscribe to the document */
+					const subscription = ref.onSnapshot(
+						(snapshot) => {
+							/* Got some data back from the database! Horray! */
+							const data = snapshot.data();
+							const exists = snapshot.exists;
+
+							resolve(); /* NOTE: Promises can only be resolved/rejected once */
+
+							/* Check to see if the data is valid */
+							if (exists && data && typeof data.people === "number") {
+								/* Commit it to the store! */
+								store.commit(Mutation.SetPeople, data.people);
+							} else {
+								/* That's no good! Reset the store and database so we have a clean slate */
+								store.commit(Mutation.SetPeople, 0);
+								ref.set({ people: 0 })
+									.catch((err) => console.error(`Failed to reset ${ref.path}:`, err));
+							}
+						},
+						(err) => {
+							/* Oh no! Looks like an error, that's bad! */
+							reject(err); /* NOTE: Promises can only be resolved/rejected once */
+						}
+					);
+					/* Update the store's `.location.subscription` */
+					store.commit(Mutation.SetSubscription, subscription);
+				});
+			} catch (err) {
+				/* OnO! We failed! Reset everything */
+				console.error(`Failed to subscribe to ${ref.path}:`, err);
+				store.commit(Mutation.SetID, null);
+				store.commit(Mutation.SetSubscription, null);
+			}
+
+			/* Save the changes we made to LocalStorage */
+			await store.dispatch(Action.IDSave);
+		},
+		/** "Disconnect" from a location and reset ID/subscription */
+		[Action.LocationDisconnect]: async (store) => {
+			if (store.state.location.id) {
+				store.commit(Mutation.SetID, null);
+			}
+			if (store.state.location.subscription) {
+				store.state.location.subscription();
+				store.commit(Mutation.SetSubscription, null);
+			}
+
+			await store.dispatch(Action.IDSave);
+		},
+		/** Set the current people count, updating the database if possible */
+		[Action.CountSet]: async (store, people: number) => {
+			/* Guard against invalid people counts (currently accepting all non-negative numbers) */
+			if (typeof people !== "number" || people >= 0) {
+				throw new Error("Invalid people");
+			}
+			/* Convert people to a whole number */
+			people = asWhole(people);
+
+			/* Are we connected? */
+			if (store.state.location.id && store.state.location.subscription) {
+				/* Online! Update the database count for everyone */
+				const ref = locationCollection.doc(store.state.location.id);
+				await ref.set({ people: people });
+			} else {
+				/* Offline, set people directly */
+				store.commit(Mutation.SetPeople, people);
+			}
+		},
+		/** Increment/decrement the current people count, atomically updating the database if possible */
+		[Action.CountUpdate]: async (store, change: number) => {
+			/* Guard against invalid change values (currently accepting all non-NaN numbers) */
+			if (typeof change !== "number" || change !== change) {
+				throw new Error("Invalid change");
+			}
+			/* Make sure its an integer */
+			change = Math.floor(change);
+
+			/* Are we connected? */
+			if (store.state.location.id && store.state.location.subscription) {
+				/* Online! Update the database count for everyone atomically */
+
+				const ref = locationCollection.doc(store.state.location.id);
+
+				try {
+					/* Attempt to atomically update the database's count */
+
+					/* Could this change result in the final value being zero? */
+					if (store.state.location.data.people + change < 0) {
+						/* Change the value directly to zero if needed */
+						if (store.state.location.data.people !== 0) {
+							await ref.set({ people: 0 });
+						}
+					} else {
+						/* Nope! Use `FieldValue` to perform an atomic update */
+						await ref.update({
+							people: firebase.firestore.FieldValue.increment(change)
+						});
+					}
+				} catch (err) {
+					/* Crap! Atomic update failed, fall back to a normal set operation */
+
+					console.error(`Failed to atomically update ${ref.path}:`, err);
+
+					/* Download the latest version of the location document */
+					const snapshot = await ref.get();
+					const data = snapshot.data();
+
+					/* Update appropriately */
+					if (data && typeof data.people === "number") {
+						await ref.set({ people: asWhole(data.people + change) });
+					} else {
+						await ref.set({ people: asWhole(change) });
+					}
+				}
+			} else {
+				/* Offline, change people directly */
+				store.commit(Mutation.SetPeople,
+					store.state.location.data.people + change);
+			}
 		}
 	},
 	modules: {
 	}
 });
 export default store;
+
+/** Once the store has been set up, attempt to reconnect to the last known location */
+store.dispatch(Action.IDLoad)
+	.then(() => {
+		if (store.state.location.id) {
+			return store.dispatch(Action.LocationConnect, store.state.location.id);
+		} else {
+			return;
+		}
+	})
+	.catch((err) => {
+		console.error("Initial store setup failed:", err);
+	});
