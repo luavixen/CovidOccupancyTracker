0a88ea2d19d853b51e038e6a84120b96f2a91ac4
Lua MacDougall <luawhat@gmail.com>
2020-06-06 17:28:43 -0700

    Added base functionality to store
    store.ts has been updated so that it actually does something, including a fully fleshed-out State and backing up of the current location ID to LocalStorage.
    Note that no actual database functionality has been implemented as of yet.

--- src/store.ts
+++ src/store.ts
@@ -7,26 +7,105 @@ import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+/* Import the Firebase SDK */
+import firebase from "firebase/app";
+/** Reference to the "locations" Firestore collection */
+export const locationCollection = firebase.firestore().collection("locations");
+
+/** LocalStorage key that contains the last valid ID */
+export const localStorageIDKey = "covid-occupancy-tracker-last-id";
+
+/** Small utility function to convert any (non-NaN) number to a natural number */
+export const asNatural = (num: number) =>
+	Math.max(Math.floor(num), 0);
+
+/** In-database representation of location data */
+export interface DatabaseLocation {
+	/** The number of people at this location */
+	people: number;
+}
+
 /** State structure */
 export interface State {
+	/** Information about the current location */
+	location: {
+		/** Location data, including person count */
+		data: DatabaseLocation;
+
+		/** ID of this location in the database */
+		id: string | null;
+		/** Subscription reference for the snapshot listener */
+		subscription: (() => void) | null;
+	}
 }
 
 /** Mutation enum, used by store.commit() <sync> */
 export enum Mutation {
+	SetPeople	= "set:location/data/people",
+	SetID		= "set:location/id",
+	SetSubscription	= "set:location/subscription"
 }
 /** Action enum, used by store.dispatch() <async> */
 export enum Action {
+	IDSave			= "idSave",
+	IDLoad			= "idLoad",
+
+	LocationConnect		= "locationConnect",
+	LocationDisconnect	= "locationDisconnect",
+
+	CountSet		= "countSet",
+	CountUpdate		= "countUpdate"
 }
 
 /** Global COT Vuex store instance */
 export const store = new Vuex.Store<State>({
 	state: {
+		location: {
+			data: { people: 0 },
+			id: null,
+			subscription: null
+		}
 	},
 	getters: {
 	},
 	mutations: {
+		/** Set the `.location.data.people` field to a natural number */
+		[Mutation.SetPeople](state, people: State["location"]["data"]["people"]) {
+			state.location.data.people = asNatural(people);
+		},
+		/** Set the `.location.id` field to a valid ID or `null` */
+		[Mutation.SetID](state, id: State["location"]["id"]) {
+			state.location.id = id;
+		},
+		/** Set the `.location.subscription` field to a valid subscription or `null` */
+		[Mutation.SetSubscription](state, subscription: State["location"]["subscription"]) {
+			state.location.subscription = subscription;
+		}
 	},
 	actions: {
+		/** Save the current `.location.id` field to LocalStorage */
+		[Action.IDSave]: async (store) => {
+			try {
+				if (store.state.location.id) {
+					localStorage.setItem(localStorageIDKey, store.state.location.id);
+				} else {
+					localStorage.removeItem(localStorageIDKey);
+				}
+			} catch (err) {
+				console.error(`Failed to update "${localStorageIDKey}" in LocalStorage:`, err);
+			}
+		},
+		/** Load the last `.location.id` field from LocalStorage */
+		[Action.IDLoad]: async (store, overwrite: boolean = false) => {
+			try {
+				const item = localStorage.getItem(localStorageIDKey);
+				if (item || overwrite) {
+					store.commit(Mutation.SetID, item);
+				}
+			} catch (err) {
+				console.error(`Failed to read "${localStorageIDKey}" from LocalStorage:`, err);
+			}
+		}
 	},
 	modules: {
 	}
